155d2134b51de74cc77dda3936ebf0d2283e2489
volkanosman <volkanonv@gmail.com>
2024-02-22 22:07:23 -0800

    added blood splatter. some tidy-up was made in the prefabs folder.

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,17 +1,33 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class Health : MonoBehaviour
 {
+    public Action onDeath;
+
     [SerializeField] private int _startingHealth = 3;
     [SerializeField] private GameObject _splatterPrefab;
+    [SerializeField] private GameObject _deathVFX;
+    
 
     private int _currentHealth;
 
     private void Start() {
         ResetHealth();
     }
+
+    private void OnEnable()
+    {
+        onDeath += SpawnDeathSplatterPrefab;
+        onDeath += SpawnDeathVFX;
+    }
+    private void OnDisable()
+    {
+        onDeath -= SpawnDeathSplatterPrefab;
+        onDeath -= SpawnDeathVFX;
+    }
+
 
     public void ResetHealth() {
         _currentHealth = _startingHealth;
@@ -21,7 +37,7 @@ public class Health : MonoBehaviour
         _currentHealth -= amount;
 
         if (_currentHealth <= 0) {
-            SpawnDeathSplatterPrefab();
+            onDeath?.Invoke();
             Destroy(gameObject);
         }
     }
@@ -34,4 +50,13 @@ public class Health : MonoBehaviour
         Color currentColor = colorChanger.DefaultColor;
         deathSplatterSpriteRenderer.color = currentColor;
      }
+
+    private void SpawnDeathVFX()
+    {
+        GameObject deathVFX = Instantiate(_deathVFX, transform.position, transform.rotation);
+        ParticleSystem.MainModule ps = deathVFX.GetComponent<ParticleSystem>().main;
+        ColorChanger colorChanger = GetComponent<ColorChanger>();
+        Color currentColor = colorChanger.DefaultColor;
+        ps.startColor = currentColor;
+    }
 }
