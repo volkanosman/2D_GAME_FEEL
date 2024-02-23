8ba55b1d7a4e2d89a0ab0edaa3b1dfec4d3cbaf3
volkanosman <volkanonv@gmail.com>
2024-02-22 16:46:32 -0800

    Added deathsplatter. When enemy gets killed it leaves blood splatter on the ground.

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private int _startingHealth = 3;
+    [SerializeField] private GameObject _splatterPrefab;
 
     private int _currentHealth;
 
@@ -20,7 +21,17 @@ public class Health : MonoBehaviour
         _currentHealth -= amount;
 
         if (_currentHealth <= 0) {
+            SpawnDeathSplatterPrefab();
             Destroy(gameObject);
         }
     }
+
+    private void SpawnDeathSplatterPrefab()
+    {
+        GameObject newSplatterPrefab = Instantiate(_splatterPrefab, transform.position, transform.rotation);
+        SpriteRenderer deathSplatterSpriteRenderer = newSplatterPrefab.GetComponent<SpriteRenderer>();
+        ColorChanger colorChanger = GetComponent<ColorChanger>();
+        Color currentColor = colorChanger.DefaultColor;
+        deathSplatterSpriteRenderer.color = currentColor;
+     }
 }
