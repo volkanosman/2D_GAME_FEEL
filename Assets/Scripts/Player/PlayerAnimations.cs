1e64b08e7852fc55f59726e0bb4b866469faa2f0
volkanosman <volkanonv@gmail.com>
2024-02-26 13:38:31 -0800

    added a hat and a tilting effect to make both the hat and the player's body move in the opposite direction when the player moves in game.

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PlayerAnimations : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _moveDustVFX;
+    [SerializeField] private float _tiltAngle = 20f;
+    [SerializeField] private float _tiltSpeed = 5f;
+    [SerializeField] private float _cowboyHatTiltModifier = 2f;
+    [SerializeField] private Transform _characterSpriteTransform;
+    [SerializeField] private Transform _hatSpriteTransform;
 
     private void Update()
     {
         DetectMoveDust();
+        ApplyTilt();
     }
 
     private void DetectMoveDust()
@@ -30,5 +36,36 @@ public class PlayerAnimations : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void ApplyTilt()
+    {
+        float targetAngle;
+
+        if(PlayerController.Instance.MoveInput.x < 0f)
+        {
+            targetAngle = _tiltAngle;
+        }else if(PlayerController.Instance.MoveInput.x > 0f)
+        {
+            targetAngle = -_tiltAngle;
+        }
+        else
+        {
+            targetAngle = 0f;
+        }
+        //PlayerHat
+        Quaternion currentCharacterRotation = _characterSpriteTransform.rotation;
+        Quaternion targetCharacterRotation = 
+            Quaternion.Euler(currentCharacterRotation.eulerAngles.x, currentCharacterRotation.eulerAngles.y, targetAngle); 
+
+        _characterSpriteTransform.rotation = Quaternion.Lerp(currentCharacterRotation,targetCharacterRotation,_tiltSpeed*Time.deltaTime);
+
+        //CowboyHat
+        Quaternion currentHatRotation = _hatSpriteTransform.rotation;
+        Quaternion targetHatRotation =
+            Quaternion.Euler(currentHatRotation.eulerAngles.x, currentHatRotation.eulerAngles.y, -targetAngle / _cowboyHatTiltModifier);
+
+        _hatSpriteTransform.rotation =
+            Quaternion.Lerp(currentHatRotation, targetHatRotation, _tiltSpeed * _cowboyHatTiltModifier * Time.deltaTime);
     }
 }
